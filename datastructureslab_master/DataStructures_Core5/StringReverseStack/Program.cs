e2d846bc71913c185d4250f8d92a596063e5d7be
urbent <urbent@my.lanecc.edu>
2026-05-26 18:45:58 -0700

    finished stringreverse
    Completed String Reverse using stack

--- datastructureslab_master/DataStructures_Core5/StringReverseStack/Program.cs
+++ datastructureslab_master/DataStructures_Core5/StringReverseStack/Program.cs
@@ -7,7 +7,29 @@ namespace StringReverseStack
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter a string to reverse: ");
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("You entered an empty string.");
+                return;
+            }
+
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in input)
+            {
+                stack.Push(c);
+            }
+
+            string reversed = "";
+            while (stack.Count > 0)
+            {
+                reversed += stack.Pop();
+            }
+
+            Console.WriteLine($"Original: {input}");
+            Console.WriteLine($"Reversed: {reversed}");
         }
     }
 }
